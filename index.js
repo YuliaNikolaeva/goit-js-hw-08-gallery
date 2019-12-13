26d6ad9e06b384436107a9f28b2fb1235009c65a
Yulia Nikolaeva <n7454@meta.ua>
2019-12-13 23:46:07 +0200

    HW-08

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict'
+
+import galleryItems from "./js/gallery-items.js";
+import ref from "./js/ref.js";
+
+const createMarkup = ({preview, original, description}) => 
+`<li class="gallery__item">
+<a
+  class="gallery__link"
+  href= ${original}
+>
+  <img
+    class="gallery__image"
+    src=${preview}
+    data-source=${original}
+    alt=${description}
+  />
+</a>
+</li>`;
+
+const createGalery = (galleryItems) => galleryItems.map((item) => 
+createMarkup(item)).join('');
+
+ref.ulGallery.insertAdjacentHTML('beforeend', createGalery(galleryItems));
+
+const showImg = (e) => {
+  e.preventDefault();
+    ref.lightBox.classList.add('is-open');
+    ref.bigImg.classList.add('active');
+    ref.bigImg.src = e.target.dataset.source;
+};
+
+const closeImg = (e) => {
+  ref.lightBox.classList.remove('is-open');
+  ref.bigImg.classList.remove('active');
+  ref.bigImg.src = '';
+};
+
+
+const controlGallery = (e) => {
+
+  if(e.target.classList.contains("gallery__image")
+  && !ref.lightBox.classList.contains('is-open')) {
+    showImg(e)
+  };
+
+  if (ref.lightBox.classList.contains('is-open') 
+  && e.target === ref.closeBtn
+  || e.target.classList.contains("lightbox__content")
+  || e.code === 'Escape') {
+      closeImg(e);
+  };
+};
+
+
+document.addEventListener('click', controlGallery);
+document.addEventListener("keydown", controlGallery);
+
+
---
+++ js/ref.js
@@ -0,0 +1,6 @@
+export default {
+  ulGallery: document.querySelector('.gallery'),
+  closeBtn: document.querySelector('.lightbox__button[data-action="close-lightbox"]'),
+  lightBox: document.querySelector('.lightbox'),
+    bigImg: document.querySelector('.lightbox > .lightbox__content > .lightbox__image'),
+};
